a1912e931f77db4f5fb068f2fc705462efcad548
cuombai <curtisombai@outlook.com>
2026-01-26 16:31:49 +0300

    Made it

--- handlers/login.go
+++ handlers/login.go
@@ -21,7 +21,9 @@ func init() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	uri := os.Getenv("MONGODB_URI")
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
