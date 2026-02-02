ec858bee806aa20ade6f70318958683403fc105d
cuombai <curtisombai@outlook.com>
2026-02-02 10:45:18 +0300

    yes

--- handlers/login.go
+++ handlers/login.go
@@ -5,6 +5,7 @@ import (
     "net/http"
     "os"
     "time"
+    "log"
 
     "eves-diary/models"
     "eves-diary/database"
@@ -20,29 +21,44 @@ func InitLoginHandlers(){
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+    log.Println("LoginHandler called")
+
     var creds struct {
         Email    string `json:"email"`
         Password string `json:"password"`
     }
     if err := json.NewDecoder(r.Body).Decode(&creds); err != nil {
+        log.Println("Decode error:", err)
         http.Error(w, "Invalid request", http.StatusBadRequest)
         return
     }
 
+    log.Println("Looking up user:", creds.Email)
     var user models.Admin
     err := userCollection.FindOne(r.Context(), bson.M{"email": creds.Email}).Decode(&user)
-    if err != nil || creds.Password != user.Password {
+    if err != nil {
+        log.Println("DB error:", err)
+        http.Error(w, "Unauthorized", http.StatusUnauthorized)
+        return
+    }
+
+    if creds.Password != user.Password {
+        log.Println("Password mismatch")
         http.Error(w, "Unauthorized", http.StatusUnauthorized)
         return
     }
 
     token, err := GenerateJWT(user.Email)
     if err != nil {
+        log.Println("JWT error:", err)
         http.Error(w, "Internal server error", http.StatusInternalServerError)
         return
     }
+
+    log.Println("Login successful for:", user.Email)
     json.NewEncoder(w).Encode(map[string]string{"token": token})
 }
+
 
 func GenerateJWT(email string) (string, error) {
     secret := os.Getenv("JWT_SECRET")
